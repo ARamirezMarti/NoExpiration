36ca32f43c62f107ad5507c37dd71e7e59f88314
tony <tony@pop-os.localdomain>
2022-08-20 23:03:11 +0200

    Se modifica los mensajes de excepcion que se devuelven al cliente

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -9,82 +9,79 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    
 
-    public function register(Request $request){
-            try{
-        $validated= $request->validate([
-                'email'=>'required|email',
-                'name'=>'required',
-                'lastname'=>'required',
-                'password'=>'required|min:8',
-            ],[
+    public function register(Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'email' => 'required|email|unique:users',
+                'name' => 'required',
+                'lastname' => 'required',
+                'password' => 'required|min:8',
+            ], [
                 'name.required' => 'Name field is required.',
                 'password.required' => 'Password field is required.',
                 'email.required' => 'Email field is required.',
                 'email.email' => 'Email field must be email address.',
-                'password.min'=> 'Password has to be 8 character at least'
+                'email.unique' => 'That email is already in use ',
+
+                'password.min' => 'Password has to be 8 character at least',
             ]);
             $validated['password'] = Hash::make($validated['password']);
 
             User::create($validated);
 
-        }catch(Exception $e){
-            
-           if(isset($e->errorInfo[1])){
+            return response()->json([
+                'status' => 1,
+                'msg' => 'User created',
 
-               $error_code=$e->errorInfo[1];
-               
-               switch ($error_code) {
-                   case '1062':
-                       return response()->json([
-                           'status' => 0,
-                           'msg' => 'That email is already in use   .',                     
-                                                   
-                           'error_code'=>$e->errorInfo[1]
-                       ]);
-                       break;
-                   
-                   default:
-                       break;
-               } 
-           }else{
+            ]);
 
-               return response()->json([
-                   'status' => 0,
-                   'msg' => $e->getMessage(),
-               ]);
-           }
+        } catch (Exception $e) {
+
+            return response()->json([
+                'status' => 0,
+                'msg' => $e->getMessage(),
+            ]);
+
         }
-        return response()->json([
-            'status' => 1,
-            'msg' => 'User created',
-        ]);
+
     }
 
-    public function login(Request $request){
-        $request->validate([
-            'email'=>'required|email',
-            'password'=>'required'
-        ]);
-      
-        $user = User::where('email','=',$request->email)->first();
-        if( isset($user->id)){
-            if(Hash::check($request->password,$user->password)){
-                $token= $user->createToken('auth_token')->plainTextToken;
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ], [
+                'email.required' => 'Email field is required.',
+                'email.email' => 'Email field must be email address.',
+                'password.required' => 'Password is required',
+            ]);
 
-                return response()->json([
+            $user = User::where('email', '=', $request->email)->first();
+
+            if (isset($user->id)) {
+                if (Hash::check($request->password, $user->password)) {
+                    $token = $user->createToken('auth_token')->plainTextToken;
+                    return response()->json([
                         'status' => 1,
-                        'token'=>$token,
-                    ],200);
+                        'token' => $token,
+                    ], 200);
+                }
             }
+            return response()->json([
+                'status' => 0,
+                'msg' => "Email or password are incorrect",
+            ], 401);
+
+        } catch (\Throwable$th) {
+            return response()->json([
+                'status' => 0,
+                'msg' => $th->getMessage(),
+            ]);
         }
-        return response()->json([
-            'status' => 0,
-            'msg' => "Email or password are incorrect",
-        ]);
- 
-    
-        
+
     }
 }
