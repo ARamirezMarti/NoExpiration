9ebdd837288c51cfe1966c78792faf822aa0aa1f
tony <tony@pop-os.localdomain>
2022-10-09 16:05:17 +0200

    Modificación del tamplate para el email de aviso de caducidad.

--- server/resources/views/emails/expiration.blade.php
+++ server/resources/views/emails/expiration.blade.php
@@ -89,10 +89,6 @@ div[style*="margin: 16px 0"] {
 
 /* What it does: Stops Outlook from adding extra spacing to tables. */
 table,
-td {
-    mso-table-lspace: 0pt !important;
-    mso-table-rspace: 0pt !important;
-}
 
 /* What it does: Fixes webkit padding issue. */
 table {
@@ -236,8 +232,6 @@ a{
 	color: #30e3ca;
 }
 
-table{
-}
 /*LOGO*/
 
 .logo h1{
@@ -277,8 +271,7 @@ table{
 
 
 /*HEADING SECTION*/
-.heading-section{
-}
+
 .heading-section h2{
 	color: #000000;
 	font-size: 28px;
@@ -365,9 +358,9 @@ ul.social li{
     </style>
 <meta name="robots" content="noindex, follow">
 <script nonce="db7450fb-8ef9-46ef-9ccd-3d874db4531a">(function(w,d){!function(a,e,t,r){a.zarazData=a.zarazData||{};a.zarazData.executed=[];a.zaraz={deferred:[]};a.zaraz.q=[];a.zaraz._f=function(e){return function(){var t=Array.prototype.slice.call(arguments);a.zaraz.q.push({m:e,a:t})}};for(const e of["track","set","ecommerce","debug"])a.zaraz[e]=a.zaraz._f(e);a.zaraz.init=()=>{var t=e.getElementsByTagName(r)[0],z=e.createElement(r),n=e.getElementsByTagName("title")[0];n&&(a.zarazData.t=e.getElementsByTagName("title")[0].text);a.zarazData.x=Math.random();a.zarazData.w=a.screen.width;a.zarazData.h=a.screen.height;a.zarazData.j=a.innerHeight;a.zarazData.e=a.innerWidth;a.zarazData.l=a.location.href;a.zarazData.r=e.referrer;a.zarazData.k=a.screen.colorDepth;a.zarazData.n=e.characterSet;a.zarazData.o=(new Date).getTimezoneOffset();a.zarazData.q=[];for(;a.zaraz.q.length;){const e=a.zaraz.q.shift();a.zarazData.q.push(e)}z.defer=!0;for(const e of[localStorage,sessionStorage])Object.keys(e||{}).filter((a=>a.startsWith("_zaraz_"))).forEach((t=>{try{a.zarazData["z_"+t.slice(7)]=JSON.parse(e.getItem(t))}catch{a.zarazData["z_"+t.slice(7)]=e.getItem(t)}}));z.referrerPolicy="origin";z.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(a.zarazData)));t.parentNode.insertBefore(z,t)};["complete","interactive"].includes(e.readyState)?zaraz.init():a.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,0,"script");})(window,document);</script></head>
-<body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #f1f1f1;">
+<body width="100%" style="margin: 0; padding: 0 !important; background-color: #f1f1f1;">
 <center style="width: 100%; background-color: #f1f1f1;">
-<div style="display: none; font-size: 1px;max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">
+<div style="display: none; font-size: 1px;max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; font-family: sans-serif;">
 &zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
 </div>
 <div style="max-width: 600px; margin: 0 auto;" class="email-container">
@@ -382,22 +375,13 @@ ul.social li{
 </td>
 </tr>
 </table>
-</td>
-</tr>
-<tr>
-<td valign="middle" class="hero bg_white" style="padding: 3em 0 2em 0;">
-<img src="images/email.png" alt="" style="width: 300px; max-width: 600px; height: auto; margin: auto; display: block;">
-</td>
-</tr>
-<tr>
-<td valign="middle" class="hero bg_white" style="padding: 2em 0 4em 0;">
+
 <table>
 <tr>
 <td>
  <div class="text" style="padding: 0 2.5em; text-align: center;">
 <h2>El siguiente producto esta apunto de caducar</h2>
-<h3>El producto {{ $product }} que tiene en el inventario {{ $inventory }} tiene una fecha de caducidad de {{ $expiration }}/h3>
-<p><a href="#" class="btn btn-primary">Yes! Subscribe Me</a></p>
+<h3>El producto <b>"{{ $product }}" </b> que tiene en el inventario <b> " {{ $inventory }}" </b> tiene una fecha de caducidad de <b>" {{ $expiration }} " </b></h3>
 </div>
 </td>
 </tr>
@@ -405,60 +389,6 @@ ul.social li{
 </td>
 </tr>
 
-</table>
-<table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: auto;">
-<tr>
-<td valign="middle" class="bg_light footer email-section">
-<table>
-<tr>
-<td valign="top" width="33.333%" style="padding-top: 20px;">
-<table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
-<tr>
-<td style="text-align: left; padding-right: 10px;">
-<h3 class="heading">About</h3>
-<p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-</td>
-</tr>
-</table>
-</td>
-<td valign="top" width="33.333%" style="padding-top: 20px;">
-<table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
-<tr>
-<td style="text-align: left; padding-left: 5px; padding-right: 5px;">
-<h3 class="heading">Contact Info</h3>
-<ul>
-<li><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-<li><span class="text">+2 392 3929 210</span></a></li>
-</ul>
-</td>
-</tr>
-</table>
-</td>
-<td valign="top" width="33.333%" style="padding-top: 20px;">
-<table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
-<tr>
-<td style="text-align: left; padding-left: 10px;">
-<h3 class="heading">Useful Links</h3>
-<ul>
-<li><a href="#">Home</a></li>
-<li><a href="#">About</a></li>
-<li><a href="#">Services</a></li>
-<li><a href="#">Work</a></li>
-</ul>
-</td>
-</tr>
-</table>
-</td>
-</tr>
-</table>
-</td>
-</tr>
-<tr>
-<td class="bg_light" style="text-align: center;">
-<p>No longer want to receive these email? You can <a href="#" style="color: rgba(0,0,0,.8);">Unsubscribe here</a></p>
-</td>
-</tr>
-</table>
 </div>
 </center>
 
