930593bdd7597a410eef1f70a953b84debd0d339
tony <tony@pop-os.localdomain>
2022-10-09 16:03:02 +0200

    Se modifica la query para  que tambien retorne infor del usuario y del inventario.

--- server/app/Models/Alerts.php
+++ server/app/Models/Alerts.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -22,9 +22,21 @@ class Alerts extends Model
 
     public static function getProductsExpirationAlerts($time)
     {
+        $selection  = '
+        CONCAT(users.name," ",users.lastname) as username,
+        product.expiration_date,
+        product.name as productName,
+        inventory.name as inventoryName
+        ';
+
         $curdate = date("Y-n-d");        
-        return self::query()->where($time.'_alert','=', $curdate)
+
+        $alerts = self::selectRaw($selection)->where($time.'_alert','=', $curdate)
+        ->join('product', 'product.id', '=', 'alerts.product_id')
+        ->join('inventory', 'inventory.id', '=', 'product.inventory_id')
+        ->join('users', 'users.id', '=', 'inventory.user_id')
         ->get();
+        return $alerts;
     }   
 
 }
